cfd5778cbdf55d95ba3ad192c3e9809d96bbd7f5
KochelaevskayaAnastasiya <nastya.kochelayevskaya.02@mail.ru>
2024-05-20 23:02:46 +0400

    Добавьте файлы проекта.

---
+++ Mindbox_test/Figure.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mindbox_test
+{
+    public interface IFigure
+    {
+        double CalculateArea();
+    }
+
+    public abstract class Figure : IFigure
+    {
+        public abstract double CalculateArea();
+    }
+
+    public class Circle : Figure
+    {
+        public double Radius { get; set; }
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public override double CalculateArea()
+        {
+            return Math.PI * Math.Pow(Radius, 2);
+        }
+    }
+
+    public class Triangle : Figure
+    {
+        public double SideA { get; set; }
+        public double SideB { get; set; }
+        public double SideC { get; set; }
+
+        public Triangle(double sideA, double sideB, double sideC)
+        {
+            //значения сторон заносятся по убыванию
+            //простейшая сортировка
+            if (sideA < sideB) (sideA, sideB) = (sideB, sideA);
+            if (sideB < sideC) (sideB, sideC) = (sideC, sideB);
+            if (sideA < sideB) (sideA, sideB) = (sideB, sideA);
+
+            this.SideA = sideA;
+            this.SideB = sideB;
+            this.SideC = sideC;
+        }
+
+        //метод для проверки треугольника на прямой угол
+        static bool IsRightTriangle(double a, double b, double c)
+        {
+            return Math.Pow(a, 2) + Math.Pow(b, 2) == Math.Pow(c, 2);
+        }
+
+        public override double CalculateArea()
+        {
+            //если треугольник прямоугольный то высчитываем по формуле 
+            if (IsRightTriangle(SideA, SideB, SideC))
+            {
+                return SideA * SideB / 2;
+            }
+            else
+            {
+                //если нет, то используем формулу Герона
+                var s = (SideA + SideB + SideC) / 2;
+                return Math.Sqrt(s * (s - SideA) * (s - SideB) * (s - SideC));
+            }
+
+        }
+    }
+}
---
+++ Mindbox_test/FigureTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Mindbox_test
+{
+    [TestFixture]
+    public class FigureTests
+    {
+        [Test]
+        public void TestCircleArea()
+        {
+            IFigure figure = new Circle(5);
+            Assert.That(Math.PI * Math.Pow(5, 2), Is.EqualTo(figure.CalculateArea()));
+        }
+
+        [TestCase(5, 4, 3, ExpectedResult = 6)]
+        [Test]
+        public void TestTriangleArea(double side1, double side2, double side3, double expectedArea)
+        {
+            IFigure figure = new Triangle(side1, side2, side3);
+            Assert.That(expectedArea, Is.EqualTo(figure.CalculateArea()));
+        }
+    }
+}
