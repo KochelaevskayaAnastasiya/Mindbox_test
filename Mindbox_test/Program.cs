65df871b708c48488b738e78cd664e7b6f0b93e5
KochelaevskayaAnastasiya <nastya.kochelayevskaya.02@mail.ru>
2024-05-21 10:14:31 +0400

    all

--- Mindbox_test/Program.cs
+++ Mindbox_test/Program.cs
@@ -1,6 +1,6 @@
 ﻿using Mindbox_test;
 //предполагается что значения корректные (не проводятся проверки на >0 и существование треугольника)
-IFigure figure = new Triangle(25, 40, 25);
+IFigure figure = new Triangle(4, 5, 3);
 
 IFigure circle = new Circle(1);
 
